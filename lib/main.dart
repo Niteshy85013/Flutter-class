c2c14c051ad017523591bcc016e5a56744ea495b
Niteshy85013 <niteshyadav85013@gmail.com>
2022-12-19 20:50:21 +0545

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:test/listtilescreen.dart';
-import 'package:test/screen/Data_tablescreen.dart';
-import 'package:test/screen/bottom_screen/dashboard.dart';
-import 'package:test/screen/bottomnavigation.dart';
+import 'package:test/screen/auth/login_screen.dart';
 
 import 'package:test/screen/display_student.dart';
-import 'package:test/screen/stack.dart';
 import 'package:test/screen/student_screen.dart';
 
 import 'arithmeticoutput.dart';
@@ -17,7 +14,7 @@ void main() {
       title: 'Flutter for class',
       initialRoute: '/',
       routes: {
-        '/': (context) => dashboardscreen(),
+        '/': (context) => const loginscreen(),
         '/studentDetailsScreen': (context) => const StudentDetailsScreen(),
         '/displaystudentscreen': (context) => const displaystudentscreen(),
 
